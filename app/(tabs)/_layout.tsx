e6dc1b621c9044f7b8eaca368fc14420ce4f6b30
RohanHemlo <rohanrahman075@gmail.com>
2026-02-10 20:14:20 +0000

    Added some icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,29 @@
 import { Tabs } from "expo-router";
+import { Sprout, UserRound } from 'lucide-react-native';
 
 export default function RootLayout() {
   return (
     <Tabs
     >
       <Tabs.Screen
-        name="main_plant" 
+        name="main_plant"
         options={{
-          title: 'My Plants'
+          title: 'My Plants',
+          tabBarIcon: () => <Sprout />,
+          // tabBarInactiveTintColor: '#45b65f',
+          tabBarActiveTintColor: '#45b65f',
         }}
-        />
-      <Tabs.Screen name="profile" />
+      />
+      <Tabs.Screen name="plant_graphs" />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: () => <UserRound />,
+          // tabBarInactiveTintColor: '#45b65f',
+          tabBarActiveTintColor: '#45b65f',
+        }}
+      />
     </Tabs>
   );
 
---
+++ app/(tabs)/plant_graphs.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function PlantGraphs() {
+
+    return (
+        <View>
+            <Text>Hi</Text>
+        </View>
+    )
+}
